b96a45f569afaa4e1f08ebc28dfaf0c7ca3cd24c
Omar Faruqe <faruqe@omars-mbp.wireless.concordia.ca>
2015-08-25 10:21:35 -0400

    Initial Commit

---
+++ CustomMapPinCalloutBubble/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomMapPinCalloutBubble
+//
+//  Created by Omar Faruqe on 2015-08-25.
+//  Copyright (c) 2015 Omar Faruqe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
